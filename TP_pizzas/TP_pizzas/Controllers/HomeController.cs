2a7a6f594c765a2af08771166d534ad521303bb2
jesikaAz <j.anschutz@laposte.net>
2020-03-19 00:31:06 +0100

    add views + add  Pizzacontroller
    generate view pizza -  create / delete / edit

--- TP_pizzas/TP_pizzas/Controllers/HomeController.cs
+++ TP_pizzas/TP_pizzas/Controllers/HomeController.cs
@@ -3,28 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TP_pizzas.Models;
 
 namespace TP_pizzas.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ TP_pizzas/TP_pizzas/Controllers/PizzaController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP_pizzas.Models;
+
+namespace TP_pizzas.Controllers
+{
+    public class PizzaController : Controller
+    {
+        //private static List<Pizza> pizzas;
+
+        // GET: Pizza
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Pizza/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Pizza/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pizza/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pizza/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Pizza/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pizza/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Pizza/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
