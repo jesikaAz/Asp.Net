37840e0c45f569c378101490352440aee29628d5
jesikaAz <j.anschutz@laposte.net>
2020-03-19 00:46:26 +0100

    move folder BO

---
+++ TP_pizzas/TP_pizzas/Models/PizzaVM.cs
@@ -0,0 +1,22 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP_pizzas.Models
+{
+    public class PizzaVM
+    {
+        public Pizza pizza { get; set; }
+
+        public List<Ingredient> ingredients = new List<Ingredient>();
+
+        public List<Pate> pates = new List<Pate>();
+
+        public List<int> selectedIngredients { get; set; } = new List<int>();
+
+        public int selectedPate { get; set; }
+    }
+}
