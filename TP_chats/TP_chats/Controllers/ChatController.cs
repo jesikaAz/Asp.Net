1284e8b0eb07ee6cb9fcbcc406228fd9159a6d40
jesikaAz <j.anschutz@laposte.net>
2020-03-18 16:03:14 +0100

    add methods details / delete / all
    and and route on layout

--- TP_chats/TP_chats/Controllers/ChatController.cs
+++ TP_chats/TP_chats/Controllers/ChatController.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TP_Chat.Models;
 
 namespace TP_chats.Controllers
 {
     public class ChatController : Controller
     {
+        private static List<Chat> meuteDeChats = Chat.GetMeuteDeChats();
+       
         // GET: Chat
         public ActionResult Index()
         {
-            return View();
+            return View(meuteDeChats);
         }
 
         // GET: Chat/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var chat = meuteDeChats.FirstOrDefault(c => c.Id == id);
+            return View(chat);
         }
 
         // GET: Chat/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var chat = meuteDeChats.FirstOrDefault(c => c.Id == id);
+            return View(chat);
         }
 
         // POST: Chat/Delete/5
@@ -32,7 +37,8 @@ namespace TP_chats.Controllers
         {
             try
             {
-                // TODO: Add delete logic here
+                var chat = meuteDeChats.FirstOrDefault(c => c.Id == id);
+                meuteDeChats.Remove(chat);
 
                 return RedirectToAction("Index");
             }
