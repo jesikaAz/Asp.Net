04ae4f0e6f46e226a82d9bae825151a47de18277
jesikaAz <j.anschutz@laposte.net>
2020-03-19 19:17:51 +0100

    update FakeDbPizza

--- TP_pizzas/TP_pizzas/Utils/FakeDbPizza.cs
+++ TP_pizzas/TP_pizzas/Utils/FakeDbPizza.cs
@@ -8,12 +8,15 @@ namespace Utils
 {
     public class FakeDbPizza
     {
+          {
         private static FakeDbPizza _instance;
         static readonly object instanceLock = new object();
 
         private FakeDbPizza()
         {
-            pizzas = this.GetPizzas();
+            this.IngredientsDisponible = this.InitIngredientsDisponibles();
+            this.PatesDisponible = this.InitPatesDisponibles();
+            this.Pizzas = new List<Pizza>();
         }
 
         public static FakeDbPizza Instance
@@ -32,36 +35,55 @@ namespace Utils
             }
         }
 
-        private readonly List<Pizza> pizzas;
+        private List<Ingredient> ingredients;
+
+        public List<Ingredient> IngredientsDisponible
+        {
+            get { return ingredients; }
+            private set { this.ingredients = value; }
+        }
+
+        private List<Pate> pates;
+
+        public List<Pate> PatesDisponible
+        {
+            get { return pates; }
+            private set { this.pates = value; }
+        }
+
+        private List<Pizza> pizzas;
 
         public List<Pizza> Pizzas
         {
             get { return pizzas; }
+            private set { pizzas = value; }
         }
 
-        private List<Pizza> GetPizzas()
+
+        private List<Ingredient> InitIngredientsDisponibles()
         {
-            return new List<Pizza>
-            {
-                new Pizza{
-                    Id=1,
-                    Nom = "Hawaienne",
-                    Pate=Pizza.PatesDisponibles.FirstOrDefault(p => p.Id == 1),
-                    Ingredients= new List<Ingredient>{
-                        Pizza.IngredientsDisponibles.FirstOrDefault(i=> i.Id== 5),
-                        Pizza.IngredientsDisponibles.FirstOrDefault(i => i.Id == 8)
-                    }
-                },
-                 new Pizza{
-                    Id=2,
-                    Nom = "Marguerita",
-                    Pate=Pizza.PatesDisponibles.FirstOrDefault(p => p.Id == 3),
-                    Ingredients= new List<Ingredient>{
-                        Pizza.IngredientsDisponibles.FirstOrDefault(i=> i.Id== 7),
-                        Pizza.IngredientsDisponibles.FirstOrDefault(i => i.Id == 1)
-                    }
-                }
-            };
+            List<Ingredient> result = new List<Ingredient>();
+            result.Add(new Ingredient { Id = 1, Nom = "Mozzarella" });
+            result.Add(new Ingredient { Id = 2, Nom = "Jambon" });
+            result.Add(new Ingredient { Id = 3, Nom = "Tomate" });
+            result.Add(new Ingredient { Id = 4, Nom = "Oignon" });
+            result.Add(new Ingredient { Id = 5, Nom = "Cheddar" });
+            result.Add(new Ingredient { Id = 6, Nom = "Saumon" });
+            result.Add(new Ingredient { Id = 7, Nom = "Champignon" });
+            result.Add(new Ingredient { Id = 8, Nom = "Poulet" });
+
+            return result;
+        }
+
+        private List<Pate> InitPatesDisponibles()
+        {
+            List<Pate> result = new List<Pate>();
+            result.Add(new Pate { Id = 1, Nom = "Pate fine, base crême" });
+            result.Add(new Pate { Id = 2, Nom = "Pate fine, base tomate" });
+            result.Add(new Pate { Id = 3, Nom = "Pate épaisse, base crême" });
+            result.Add(new Pate { Id = 4, Nom = "Pate épaisse, base tomate" });
+
+            return result;
         }
     }
 }
