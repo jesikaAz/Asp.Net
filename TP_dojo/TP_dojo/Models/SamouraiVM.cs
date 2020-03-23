e02b3adf5382a36df3ac31f2b77eabc0801d367a
jesikaAz <j.anschutz@laposte.net>
2020-03-23 14:54:33 +0100

    add viewmodel + update migrations

---
+++ TP_dojo/TP_dojo/Models/SamouraiVM.cs
@@ -0,0 +1,13 @@
+﻿using BO;
+using System.Collections.Generic;
+
+namespace TP_dojo.Models
+{
+    public class SamouraiVM
+        {
+            public Samourai Samourai { get; set; }
+            public List<Arme> Armes { get; set; }
+            public int? IdSelectedArme { get; set; }
+        }
+}
+
