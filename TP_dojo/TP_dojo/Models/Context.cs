4c1b079a6969fc090e7c2dd6adc0b7d4a4dbbd50
jesikaAz <j.anschutz@laposte.net>
2020-03-23 13:12:51 +0100

    add class Context + ConnectionString

---
+++ TP_dojo/TP_dojo/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TP_dojo.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<BO.Samourai> Samourais { get; set; }
+
+        public DbSet<BO.Arme> Armes { get; set; }
+
+    }
+}
