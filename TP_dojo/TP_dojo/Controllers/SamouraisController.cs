2f539c3b577f918fd5929b1e5cb16dea0b7bc321
jesikaAz <j.anschutz@laposte.net>
2020-03-24 15:01:58 +0100

    update

--- TP_dojo/TP_dojo/Controllers/SamouraisController.cs
+++ TP_dojo/TP_dojo/Controllers/SamouraisController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using BO;
 using TP_dojo.Models;
@@ -14,6 +12,12 @@ namespace TP_dojo.Controllers
     public class SamouraisController : Controller
     {
         private Context db = new Context();
+
+        // Method getArmesUsed
+        private List<int> getArmesUsed()
+        {
+            return db.Samourais.Where(s => s.Arme != null).Select(s => s.Arme.Id).ToList();
+        }
 
         // GET: Samourais
         public ActionResult Index()
@@ -33,30 +37,47 @@ namespace TP_dojo.Controllers
             {
                 return HttpNotFound();
             }
+            // calcul potentiel
+            ViewBag.Potentiel = (samourai.Force + (samourai.Arme == null ? 0 : samourai.Arme.Degats)) * (samourai.ArtsMartiaux.Count() > 0 ? samourai.ArtsMartiaux.Count() : 1);
             return View(samourai);
         }
 
         // GET: Samourais/Create
         public ActionResult Create()
         {
-            return View();
+            var armesUsed = this.getArmesUsed();
+            SamouraiVM samouraiVM = new SamouraiVM
+            {
+                Armes = db.Armes.Where(a => !armesUsed.Contains(a.Id)).ToList(),
+                ArtMartiaux = db.ArtMartials.ToList()
+            };
+            return View(samouraiVM);
         }
 
         // POST: Samourais/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Force,Nom")] Samourai samourai)
+        public ActionResult Create(SamouraiVM samouraiVM)
         {
             if (ModelState.IsValid)
             {
-                db.Samourais.Add(samourai);
+                if (samouraiVM.IdSelectedArme.HasValue)
+                {
+                    samouraiVM.Samourai.Arme = db.Armes.FirstOrDefault(a => a.Id == samouraiVM.IdSelectedArme);
+                }
+                samouraiVM.Samourai.ArtsMartiaux = new List<ArtMartial>();
+                foreach (var idArtMartial in samouraiVM.IdSelectedArtMartiaux)
+                {
+                    samouraiVM.Samourai.ArtsMartiaux.Add(db.ArtMartials.Find(idArtMartial));
+                }
+                db.Samourais.Add(samouraiVM.Samourai);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(samourai);
+            var armesUsed = this.getArmesUsed();
+            samouraiVM.Armes = db.Armes.Where(a => !armesUsed.Contains(a.Id)).ToList();
+            samouraiVM.ArtMartiaux = db.ArtMartials.ToList();
+            return View(samouraiVM);
         }
 
         // GET: Samourais/Edit/5
@@ -71,23 +92,65 @@ namespace TP_dojo.Controllers
             {
                 return HttpNotFound();
             }
-            return View(samourai);
+
+            var armesUsed = this.getArmesUsed();
+            var samouraiVM = new SamouraiVM
+            {
+                Samourai = samourai,
+                Armes = db.Armes.Where(a => !armesUsed.Contains(a.Id)).ToList(),
+                ArtMartiaux = db.ArtMartials.ToList(),
+                IdSelectedArtMartiaux = samourai.ArtsMartiaux.Select(ar => ar.Id).ToList()
+            };
+
+            if (samourai.Arme != null)
+            {
+                samouraiVM.IdSelectedArme = samourai.Arme.Id;
+                samouraiVM.Armes.Add(samourai.Arme);
+            }
+
+            return View(samouraiVM);
         }
 
         // POST: Samourais/Edit/5
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Force,Nom")] Samourai samourai)
+        public ActionResult Edit(SamouraiVM samouraiVM)
         {
+            Samourai samouraiDB = db.Samourais.Find(samouraiVM.Samourai.Id);
             if (ModelState.IsValid)
             {
-                db.Entry(samourai).State = EntityState.Modified;
+                if (samouraiDB.Arme != null)
+                {
+                    var arme = db.Armes.Find(samouraiDB.Arme.Id);
+                    db.Entry(arme).State = EntityState.Modified;
+                }
+
+                samouraiDB.Arme = null;
+                if (samouraiVM.IdSelectedArme.HasValue)
+                {
+                    samouraiDB.Arme = db.Armes.FirstOrDefault(a => a.Id == samouraiVM.IdSelectedArme);
+                }
+                samouraiDB.ArtsMartiaux.Clear();
+                if (samouraiVM.IdSelectedArtMartiaux != null)
+                {
+                    foreach (var idArtMartial in samouraiVM.IdSelectedArtMartiaux)
+                    {
+                        samouraiDB.ArtsMartiaux.Add(db.ArtMartials.Find(idArtMartial));
+                    }
+                }
+                samouraiDB.Nom = samouraiVM.Samourai.Nom;
+                samouraiDB.Force = samouraiVM.Samourai.Force;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(samourai);
+            var armesUsed = this.getArmesUsed();
+            samouraiVM.Armes = db.Armes.Where(a => !armesUsed.Contains(a.Id)).ToList();
+            samouraiVM.ArtMartiaux = db.ArtMartials.ToList();
+            if (samouraiDB.Arme != null)
+            {
+                samouraiVM.Armes.Add(samouraiDB.Arme);
+            }
+            return View(samouraiVM);
         }
 
         // GET: Samourais/Delete/5
