5fc768bc81fb9eecc6d26162c945abbbd809b3a8
jesikaAz <j.anschutz@laposte.net>
2020-03-26 15:25:12 +0100

    update DB
    fin partie2

---
+++ OgameLikeTP/OgameLikeTP/Migrations/202003261421473_Migration2.cs
@@ -0,0 +1,22 @@
+﻿namespace OgameLikeTP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migration2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Planets", "Name", c => c.String(maxLength: 20));
+            AlterColumn("dbo.Resources", "Name", c => c.String(maxLength: 20));
+            AlterColumn("dbo.SolarSystems", "Name", c => c.String(maxLength: 20));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.SolarSystems", "Name", c => c.String());
+            AlterColumn("dbo.Resources", "Name", c => c.String());
+            AlterColumn("dbo.Planets", "Name", c => c.String());
+        }
+    }
+}
